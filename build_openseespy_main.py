20d61e86ab642bb10c25e60abc83c53c1a07f73f
Minjie Zhu <zhum@oregonstate.edu>
2020-02-13 15:33:14 -0800

    update build openseespy

--- build_openseespy_main.py
+++ build_openseespy_main.py
@@ -4,11 +4,12 @@ import os
 import os.path
 import sys
 
-# remove old binary
+# user-set directories
 openseessrc = '/data2/Minjie/projects/openseespy'
 binary = openseessrc + '/SRC/interpreter/openseespy'
 pythoninstall = '/scratch/bin/openseespy'
 pythonsrc = '/scratch/bin/Python-3.7.6'
+pypidir = '/data2/Minjie/projects/openseespypip'
 
 if os.path.exists(binary):
     os.remove(binary)
@@ -41,18 +42,32 @@ if not setup:
     os.chdir(pythoninstall+'/bin')
     for program in ['2to3-3.7', 'easy_install-3.7', 'idle3.7', 'pip3', 'pip3.7', 'pydoc3.7', 'pyvenv-3.7']:
         print('Setting up', program)
-        f = open('./'+program, 'r')
-        data = f.read()
-        f.close()
-        f = open('./'+program, 'w')
-        f.write(data.replace('python3.7', 'openseespy'))
-        f.close()
+        with open('./'+program, 'r') as f:
+            data = f.read()
+        with open('./'+program, 'w') as f:
+            f.write(data.replace('python3.7', 'openseespy'))
     for program in ['python3', 'python3.7', 'python3.7m']:
         os.remove('./'+program)
 
+    # install packages
     subprocess.run(['./openseespy', '-m', 'pip', 'install', 'notebook'])
     subprocess.run(['./openseespy', '-m', 'pip', 'install', 'numpy'])
     subprocess.run(['./openseespy', '-m', 'pip', 'install', 'matplotlib'])
+
+    # install openseespy
+    os.mkdir(pythoninstall+'/lib/python3.7/site-packages/openseespy')
+    os.chdir(pythoninstall+'/lib/python3.7/site-packages/openseespy')
+    shutil.copy(pypidir+'/openseespy/version.py', '.')
+    shutil.copy(pypidir+'/openseespy/__init__.py', '.')
+    os.mkdir('opensees')
+    os.chdir('opensees')
+    with open('__init__.py', 'w') as f:
+        f.write('from opensees import *')
+    os.chdir('..')
+    os.mkdir('postprocessing')
+    os.chdir('postprocessing')
+    shutil.copy(pypidir+'/openseespy/postprocessing/Get_Rendering.py', '.')
+    shutil.copy(pypidir+'/openseespy/postprocessing/__init__.py', '.')
 
 # get all libraries
 os.chdir(pythoninstall+'/bin')
