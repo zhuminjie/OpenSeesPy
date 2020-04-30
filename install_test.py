27470597135c1b857b1dc62f0a39727c34500812
Minjie Zhu <zhum@oregonstate.edu>
2020-04-29 17:28:38 -0700

    update install scripts

--- install_test.py
+++ install_test.py
@@ -3,8 +3,8 @@ import subprocess
 from build_pip import copy_library, build_pip, upload_pip_test, clean_pip
 
 
-copy_library('../openseespy/SRC/interpreter/opensees.so',
-             '../openseespy/SRC/interpreter/opensees.pyd')
+copy_library('../OpenSees/SRC/interpreter/opensees.so',
+             '../OpenSees/SRC/interpreter/opensees.pyd')
 build_pip()
 upload_pip_test()
 clean_pip()
