7e89826a68f4e47011c4706ac9dc7a824993b7f1
Minjie Zhu <zhum@oregonstate.edu>
2020-05-07 15:56:43 -0700

    test version

--- install_pip.py
+++ install_pip.py
@@ -2,8 +2,8 @@ import subprocess
 
 from build_pip import copy_library, build_pip, upload_pip, clean_pip
 
-copy_library('../OpenSees/SRC/interpreter/opensees.so',
-             '../OpenSees/SRC/interpreter/opensees.pyd')
+copy_library('../OpenSeesPy/SRC/interpreter/opensees.so',
+             '../OpenSeesPy/SRC/interpreter/opensees.pyd')
 build_pip()
 upload_pip()
 clean_pip()
