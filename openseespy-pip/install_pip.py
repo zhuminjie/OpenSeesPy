4e25f2daf083037c47e8e49a1fd0cff0817a914c
Minjie Zhu <zhum@oregonstate.edu>
2020-08-11 07:24:56 -0700

    update install_pip.py

--- openseespy-pip/install_pip.py
+++ openseespy-pip/install_pip.py
@@ -2,8 +2,9 @@ import subprocess
 
 from build_pip import copy_library, build_pip, upload_pip, clean_pip
 
-copy_library('../OpenSeesPy/SRC/interpreter/opensees.so',
-             '../OpenSeesPy/SRC/interpreter/opensees.pyd')
+copy_library('../../opensees/SRC/interpreter/opensees.so',
+             '../../opensees/SRC/interpreter/opensees.pyd',
+             '../../opensees/SRC/interpreter/opensees37.pyd',)
 build_pip()
 upload_pip()
 clean_pip()
