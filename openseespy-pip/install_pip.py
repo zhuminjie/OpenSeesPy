caf33047e9c02100d4fb991e3a6d942d19e746fc
Minjie Zhu <zhum@oregonstate.edu>
2020-09-10 19:45:08 -0700

    update install_pip and install_test

--- openseespy-pip/install_pip.py
+++ openseespy-pip/install_pip.py
@@ -3,8 +3,7 @@ import subprocess
 from build_pip import copy_library, build_pip, upload_pip, clean_pip
 
 copy_library('../../opensees/SRC/interpreter/opensees.so',
-             '../../opensees/SRC/interpreter/opensees.pyd',
-             '../../opensees/SRC/interpreter/opensees37.pyd',)
+             '../../opensees/SRC/interpreter/opensees.pyd')
 build_pip()
 upload_pip()
 clean_pip()
--- openseespy-pip/install_test.py
+++ openseespy-pip/install_test.py
@@ -4,8 +4,7 @@ from build_pip import copy_library, build_pip, upload_pip_test, clean_pip
 
 
 copy_library('../../opensees/SRC/interpreter/opensees.so',
-             '../../opensees/SRC/interpreter/opensees.pyd',
-             '../../opensees/SRC/interpreter/opensees37.pyd',)
+             '../../opensees/SRC/interpreter/opensees.pyd')
 build_pip()
 upload_pip_test()
 clean_pip()
