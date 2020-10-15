40e506c545b39eed784df2cdf24ddf8b020b851c
Minjie Zhu <zhum@oregonstate.edu>
2020-10-15 14:23:49 -0700

    copy linux libray

--- openseespy-pip/install_test.py
+++ openseespy-pip/install_test.py
@@ -1,9 +1,9 @@
 import subprocess
 
-from build_pip import copy_library, build_pip, upload_pip_test, clean_pip
+from build_pip import copy_linux_library, build_pip, upload_pip_test, clean_pip
 
 
-copy_library('../../opensees/SRC/interpreter/opensees.so')
+copy_linux_library('../../opensees/SRC/interpreter/opensees.so')
 build_pip()
 upload_pip_test()
 clean_pip()
