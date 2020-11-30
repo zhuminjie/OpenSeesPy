a8220fcdf837f56b0a02539b91bdf8426edefbcb
Minjie Zhu <zhum@oregonstate.edu>
2020-11-30 14:58:45 -0800

    update docker

--- openseespy-docker/build_docker.py
+++ openseespy-docker/build_docker.py
@@ -5,7 +5,7 @@ import os.path
 import sys
 
 
-def build_docker(dry_run=True, push=False):
+def build_docker(push=False):
 
     # change to script's folder
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -17,6 +17,13 @@ def build_docker(dry_run=True, push=False):
 
     shutil.copytree('../openseespy-pip', 'openseespy-pip')
 
+    # opensees
+    if os.path.exists('opensees'):
+        print('removing opensees')
+        shutil.rmtree('opensees')
+
+    shutil.copytree('../opensees', 'opensees')
+
     # version
     about = {}
     with open('openseespy-pip/openseespy/version.py') as fp:
@@ -24,44 +31,33 @@ def build_docker(dry_run=True, push=False):
     version = about['version']
 
     # tag
-    tag_compile = f'compile:{version}'
-    tag_openseespy = f'zhuminjie/openseespy:{version}'
-    tag_notebook = f'zhuminjie/openseespy:{version}-notebook'
-    if not dry_run:
-        subprocess.run(['docker',
-                        'build', '--target', 'compile',
-                        '-t', tag_compile, '.'])
-        subprocess.run(['docker',
-                        'build', '--target', 'openseespy',
-                        '-t', tag_openseespy, '.'])
-        subprocess.run(['docker',
-                        'build', '--target',
-                        'openseespy_notebook',
-                        '-t', tag_notebook, '.'])
+    subprocess.run(['docker',
+                    'build', '--target', 'ubuntu-petsc',
+                    '-t', f'ubuntu-petsc:{version}', '.'])
+    subprocess.run(['docker',
+                    'build', '--target', 'ubuntu-openseespy',
+                    '-t', f'ubuntu-openseespy:{version}', '.'])
+    subprocess.run(['docker',
+                    'build', '--target', 'ubuntu-pip',
+                    '-t', f'ubuntu-pip:{version}', '.'])
+    subprocess.run(['docker',
+                    'build', '--target', 'ubuntu-install',
+                    '-t', f'zhuminjie/openseespy:{version}', '.'])
+    subprocess.run(['docker',
+                    'build', '--target', 'ubuntu-notebook',
+                    '-t', f'zhuminjie/openseespy:{version}-notebook', '.'])
 
-        if push:
-            subprocess.run(['docker', 'login'])
-            subprocess.run(['docker', 'image',
-                            'push', tag_openseespy])
-            subprocess.run(['docker', 'image',
-                            'push', tag_notebook])
-    else:
-        print(f'docker build --target compile -t {tag_compile} .')
-        print(f'docker build --target openseespy -t {tag_openseespy} .')
-        print(f'docker build --target openseespy_notebook -t {tag_notebook} .')
-
-        if push:
-            print(f'docker login')
-            print(f'docker image push {tag_openseespy}')
-            print(f'docker image push {tag_notebook}')
+    if push:
+        subprocess.run(['docker', 'login'])
+        subprocess.run(['docker', 'image',
+                        'push', tag_openseespy])
+        subprocess.run(['docker', 'image',
+                        'push', tag_notebook])
 
 
 if __name__ == "__main__":
-    dry_run = False
     push = False
     for i in range(1, len(sys.argv)):
-        if sys.argv[i] == '-dry-run':
-            dry_run = True
-        elif sys.argv[i] == '-push':
+        if sys.argv[i] == '-push':
             push = True
-    build_docker(dry_run, push)
+    build_docker(push)
